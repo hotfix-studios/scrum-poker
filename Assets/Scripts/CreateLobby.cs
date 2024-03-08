5c48e5f16c1da096913def84a9500df9ea758898
Bryan Burnside <bpburnside@gmail.com>
2024-03-08 11:39:50 -0600

    chore: update Main scene, create simple lobby, create basic socket functions and cases (init and create mostly functional), create and pass dummy user data, create button to create lobby, create input field for users to copy invite codes (roomIds), create greeting, create rooms obj to hold room data on wss

---
+++ Assets/Scripts/CreateLobby.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using NativeWebSocket;
+using System;
+using TMPro;
+using Newtonsoft.Json;
+
+public class CreateLobby : MonoBehaviour
+{
+    public class Data
+    {
+        public string type {  get; set; }
+
+        [JsonProperty("params")]
+        public Params Params { get; set; }
+    }
+    public class Params
+    {
+        public string roomId;
+        public string userId;
+    }
+
+    // private List<string> users; 
+
+    [SerializeField] private TMP_InputField _lobby;
+    [SerializeField] private TextMeshProUGUI greeting;
+    private string roomId = Guid.NewGuid().ToString().Substring(0, 5);
+
+    private void Update()
+    {
+        if (WebSocketConnection.userId != null)
+        {
+            if (!greeting.text.Contains(WebSocketConnection.userId))
+            {
+                greeting.text += WebSocketConnection.userId;
+            }
+        }
+    }
+
+    public void OnButtonClick()
+    {
+        CreateRoom();
+        _lobby.text = roomId;
+    }
+
+    async void CreateRoom()
+    {
+        if (WebSocketConnection.ws.State == WebSocketState.Open)
+        {
+            var data = new Data
+            {
+                type = "create",
+                Params = new Params
+                {
+                    roomId = roomId,
+                    userId = WebSocketConnection.userId
+                }
+            };
+            string json = JsonConvert.SerializeObject(data);
+            Debug.Log("create" + json);
+            await WebSocketConnection.ws.SendText(json);
+        }
+    }
+}
--- src/wss.ts
+++ src/wss.ts
@@ -1,3 +1,4 @@
+import { randomUUID } from "crypto";
 import dotenv from "dotenv";
 import http from "http";
 import WebSocket, { WebSocketServer } from 'ws';
@@ -7,6 +8,10 @@ dotenv.config();
 const port = process.env.WSS_PORT;
 const server = http.createServer();
 
+const rooms = {};
+
+const userId = "Dummy Testy";
+
 // Creating a websocket to run on our server
 const wss = new WebSocketServer({ server }, () => {
   console.log('WSS started');
@@ -14,11 +19,56 @@ const wss = new WebSocketServer({ server }, () => {
 
 wss.on('connection', (ws) => {
   ws.on('error', console.error);
+  ws.on('message', data => {
+    const rawData = data.toString();
+    const obj = JSON.parse(rawData);
+    const type = obj.type;
+    const params = obj.params;
 
-  ws.on('message', (data) => {
-    console.log('data received from client: ', data);
-    ws.send('Message received!');
+    switch (type) {
+      case "init":
+        init(params);
+        break;
+      case "create":
+        create(params);
+        console.log("rooms obj", rooms);
+        break;
+      case "join":
+        join(params);
+        break;
+      case "leave":
+        leave(params);
+        break;
+      default:
+          console.warn(`Type: ${type} unknown`);
+        break;
+    }
   });
+  const init = params => {
+    const obj = {
+      type: 'init',
+      params: {
+        userId
+      }
+    }
+    ws.send(JSON.stringify(obj));
+  };
+  const create = params => {
+    rooms[params.roomId] = {
+      users: [params.userId]
+    };
+  };
+  const join = params => {};
+  const leave = params => {};
+  // ws.on('message', (data) => {
+  //   console.log('data received from client: ', data.toString());
+  //   ws.send('Message received!');
+  // });
+
+  // ws.on('message', (data) => {
+  //   console.log('data received from client: ', data.toString());
+  //   ws.send(JSON.stringify(rooms));
+  // });
 
 });
 
