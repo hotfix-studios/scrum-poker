dceb8144b4cb95d6de08927186351e0aac3b66aa
Colin Williams <colin.williams.dev@gmail.com>
2024-03-10 00:00:31 -0600

    refactor!: change from http server to express, add eventHandler register call

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
-import http from "http";
+// import http from "http";
+import express from "express";
 import dotenv from "dotenv";
 import "./db/index.js";
 
 import { WebSocketServer } from "ws";
-import { middleware } from "./app.js";
+import { app, middleware } from "./app.js";
+
+/* API */
+import { registerEventListeners } from "./router/index.js";
 
 dotenv.config();
 
@@ -12,12 +16,25 @@ const host = process.env.NODE_ENV === "production" ? "0.0.0.0" : "localhost";
 const path = process.env.WEBHOOK_PATH;
 const localWebhookUrl = `http://${host}:${port}${path}`;
 
-const server = http.createServer(middleware).listen(port, () => {
+// const server = http.createServer(middleware).listen(port, () => {
+//   console.log("\x1b[34m", `Server is listening for events at: ${localWebhookUrl}`);
+//   console.log("Press Ctrl + C to quit.");
+// });
+
+const server = express();
+
+server.use(middleware);
+
+server.listen(port, () => {
   console.log("\x1b[34m", `Server is listening for events at: ${localWebhookUrl}`);
   console.log("Press Ctrl + C to quit.");
 });
 
+// app.webhooks.on("issues.opened", webhookApi.issueOpenedHandler);
+registerEventListeners(app);
+
 /* Creating a websocket to run on our server */
-const wss = new WebSocketServer({ server }, () => {
-  console.log('WSS started');
-});
+// TODO: refactor wss for Express server...
+// const wss = new WebSocketServer({ server }, () => {
+//   console.log('WSS started');
+// });
