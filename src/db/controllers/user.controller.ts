e24159c54dd056174089ed660a265bb07f0818af
Colin Williams <colin.williams.dev@gmail.com>
2024-03-11 16:09:51 -0500

    feat: first concrete instance of controller from base class

--- src/db/controllers/user.controller.ts
+++ src/db/controllers/user.controller.ts
@@ -1,23 +1,13 @@
 import { User } from "../models/index.js";
+import { ARepository } from "./base/ARepository.js";
+import { Request, Response } from "express";
+import { Schema, Model, FilterQuery } from "mongoose";
 
 /**
  * This will be the Repository for User Model (CRUD)
  */
-
-// TODO: Abstract this into an interface or base class... (modular for all models)
-const getUser = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const user = await User.findById(id);
-
-    if (!user) {
-      return res.status(404).json({ message: "User not found" });
-    }
-
-    return res.status(200).json(user);
-
-  } catch (error) {
-    console.error('Internal server error when getting user:', error);
-    res.sendStatus(500).json({ message: "Internal Server Error Get" });
+export class UserController extends ARepository {
+  constructor() {
+    super(User);
   }
 };
