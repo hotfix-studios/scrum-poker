3820e2d76953f157ab27cdd9af2d7096390fd3a2
Colin Williams <colin.williams.dev@gmail.com>
2024-03-14 00:29:30 -0500

    feat: two pre save hook middleware for host and users working, _id default again

--- src/db/models/room.model.ts
+++ src/db/models/room.model.ts
@@ -1,9 +1,31 @@
 import { Schema, model } from "mongoose";
 
+
 const roomSchema = new Schema({
-  _id: { type: Schema.Types.String, required: true, unique: true },
+  room_id: { type: Schema.Types.String, required: true, unique: true },
+  host: { type: Schema.Types.String, unique: true },
   users: [ { type: Schema.Types.String } ]
-}, { timestamps: { createdAt: "created_at", updatedAt: "updated_at" } });
+},
+{
+  timestamps: { createdAt: "created_at", updatedAt: "updated_at" },
+  optimisticConcurrency: true
+});
+
+// Pre-save middleware to ensure users is always an array
+roomSchema.pre('save', function(next) {
+  if (!Array.isArray(this.users)) {
+    this.users = [this.users]; // Convert to array if not already
+  }
+  next();
+});
+
+// Pre-save middleware to set host based on _id
+roomSchema.pre('save', function(next) {
+  if (!this.host) {
+    this.host = this.users[0];
+  }
+  next();
+});
 
 /**
  * @summary const rooms = await Room.find().lean();
