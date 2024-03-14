9fc342e818a30a049330b0b276a90089a1b8dfe1
Colin Williams <colin.williams.dev@gmail.com>
2024-03-14 00:29:43 -0500

    feat: seed Users table in DB working

--- src/db/models/index.ts
+++ src/db/models/index.ts
@@ -1,6 +1,30 @@
+import { randomBytes } from "crypto";
 import User from "./user.model.js";
+import Room from "./room.model.js";
 
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const mongoUri: string | undefined = process.env.DB_CONNECTION;
+
+console.log(mongoUri);
 console.log(User);
+
+if (mongoUri != undefined) {
+  mongoose
+    .connect(mongoUri)
+    .then(() => {
+      console.log("\x1b[36m%s\x1b[0m", "Connected to Cloud DB");
+    })
+    .catch((err) => {
+      console.error("\x1b[31m%s\x1b[0m", "Failed to connect to Cloud DB", err);
+    });
+} else {
+  console.error("\x1b[31m%s\x1b[0m", "Mongo Cloud DB Connection string is undefined");
+}
+
 
 /** ***************** **
  * ******************* *
@@ -8,7 +32,30 @@ console.log(User);
  * ******************* *
  ** ***************** **/
 
+const roomId = randomBytes(3).toString("base64").slice(0, 5);
+console.log("ROOM ID FROM BYTES:", roomId);
+const userHost = "Dummy Testy";
+const userPlayer = "Dum Dum 2";
+
+try {
+  const doc = await Room.create({ room_id: roomId, users: userHost });
+  console.log(doc);
+} catch (error) {
+  console.error("First Insert Failed", error);
+}
+
+try {
+  const docTwo = await Room.findOne( { room_id: roomId });
+  docTwo.users.push(userPlayer);
+  console.log("SECOND INSERT SUCCESS:", docTwo);
+  await docTwo.save();
+} catch (error) {
+  console.error("Second Insert Failed", error);
+}
+
 export { User };
+
+// { useNewUrlParser: true, useUnifiedTopology: true }
 
 /* Mongo Change Streams Implementation:
 async function run() {
