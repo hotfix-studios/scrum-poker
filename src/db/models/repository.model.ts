70d7039806624aa2e1d5f5e2abf097a4fa8a432c
Colin Williams <colin.williams.dev@gmail.com>
2024-03-14 20:34:39 -0500

    fix: required typo on schemas

--- src/db/models/repository.model.ts
+++ src/db/models/repository.model.ts
@@ -1,9 +1,9 @@
 import { Schema, model } from "mongoose";
 
 const repositorySchema = new Schema({
-  gh_id: { type: Schema.Types.Number, require: true },
-  name: { type: Schema.Types.String, require: true },
-  full_name: { type: Schema.Types.String, require: true },
-  private: { type: Schema.Types.Boolean, require: true },
-  owner: { type: Schema.Types.Number, require: true, ref: "User" } // use user.gh_id
+  gh_id: { type: Schema.Types.Number, required: true },
+  name: { type: Schema.Types.String, required: true },
+  full_name: { type: Schema.Types.String, required: true },
+  private: { type: Schema.Types.Boolean, required: true },
+  owner: { type: Schema.Types.Number, required: true, ref: "User" } // use user.gh_id
 });
