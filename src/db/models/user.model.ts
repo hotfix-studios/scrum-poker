4a1d599d92c279e2ea9332f990c78f7788315f7b
Colin Williams <colin.williams.dev@gmail.com>
2024-03-16 18:57:57 -0500

    feat: app owner bool tracker prop on user model

--- src/db/models/user.model.ts
+++ src/db/models/user.model.ts
@@ -5,7 +5,8 @@ const userSchema = new Schema({
   type: { type: Schema.Types.String, required: true },
   name: { type: Schema.Types.String, required: true },
   avatar_url: { type: Schema.Types.String, required: true },
-  game_host: { type: Schema.Types.Boolean, default: false }
+  game_host: { type: Schema.Types.Boolean, default: false },
+  app_owner: { type: Schema.Types.Boolean, default: false }
 }, { timestamps: { createdAt: "created_at", updatedAt: "updated_at" } });
 
 export default model("User", userSchema);
