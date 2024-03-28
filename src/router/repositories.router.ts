3acf67dd9970a35ea0bbf026fd82518c626aa4bc
Colin Williams <colin.williams.dev@gmail.com>
2024-03-27 23:48:32 -0500

    feat: route to get user.name for issues sequence

--- src/router/repositories.router.ts
+++ src/router/repositories.router.ts
@@ -9,8 +9,16 @@ const repositoriesRouter = Router();
  * @implements route /api/repos
  */
 repositoriesRouter
-  .route("/:id")
+  .route("names/:id")
   .get(octokitApi.getReposById);
 
+  /**
+   * @summary this route uses 3 middleware to hit repos and users in db, targeting `repo._id, repo.name, user.name`
+   * @returns `obj.repo_data = { _id: 1234.., name: "blah", owner_id: 1234.. }`
+   * @returns `obj.user_data = { _id: 1234.., nameL "blah-blah" }`
+   */
+repositoriesRouter
+  .route("/names/:id/:projections")
+  .get([ octokitApi.getRepoDataById, octokitApi.getUserNameByOwnerId, octokitApi.sendData ]);
 
 export default repositoriesRouter;
