8ed1a9842c5ad922c66bf5883e4a5229e91785fa
Colin Williams <colin.williams.dev@gmail.com>
2024-04-14 01:30:36 -0500

    feat: get __dirname for file, define cron job to truncate error log file at midnight, fire job

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+import cron from "cron";
 import express from "express";
 // TODO: install @types/cors
 import dotenv from "dotenv";
@@ -13,6 +16,8 @@ import { configureServer, registerEventListeners } from "./router/index.js";
 /////////////////////////////////////////////////////////
 // #region ///////////////// ENV VARS ///////////////////
 dotenv.config();
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const port = process.env.PORT;
 const host = process.env.NODE_ENV === "production" ? "0.0.0.0" : "localhost";
@@ -36,10 +41,28 @@ const server = _express.listen(port, () => {
 
 /* creating a websocket to run on our server */
 // const wss = new WebSocketServer({ server }, () => {
-//   console.log('WSS started');
+//   console.log("WSS started");
 // });
 
 registerEventListeners(app);
+
+// Define a cron job that runs every day at midnight
+const job = new cron.CronJob("0 0 0 * * *", () => {
+  // Path to your error log file
+  const errorLogFilePath = path.resolve(__dirname, "..", "error.log");
+
+  // Truncate the error log file to empty it
+  fs.truncate(errorLogFilePath, 0, (err) => {
+    if (err) {
+      console.error("Error truncating error.log file:", err);
+    } else {
+      console.log("Disk error.log file has been cleaned out.");
+    }
+  });
+});
+
+// Start the cron job
+job.start();
 /////////////////////////////////////////////////////////
 // #endregion ///////////////////////////////////////////
 
@@ -78,5 +101,5 @@ process.on('uncaughtException', (err, origin) => {
 /////////////////////////////////////////////////////////
 // #endregion ///////////////////////////////////////////
 
-/* TODO: all 3 of these need testing, idk where fs.writeSync outputs.. */
+/* TODO: all 3 of these need testing */
 /* [node critical failure handling process.on docs](https://nodejs.org/api/process.html#process_event_uncaughtexception) */
