1c9dcae4a7defd81dd7d185096047a357a7c576c
Bryan Burnside <bpburnside@gmail.com>
2024-05-02 12:22:46 -0500

    feat: add additional global state variables, add classes for Host, User, and Repo, add events and delegates for adding and removing participants, clean up logic

--- Assets/Scripts/Store.cs
+++ Assets/Scripts/Store.cs
@@ -16,21 +16,35 @@ public class Store : MonoBehaviour
     public static string code;
 
     // USER && ROOM
+    public static bool isHost = false;
     public static int id;
     public static string avatar;
     public static string fullName;
+    public static List<string> repoNames;
     public static string repoName;
     public static int repoOwnerId;
     public static string roomId;
     public static object[] issues;
 
+    public class Host
+    {
+        public string hostName;
+    }
+
     public class User
     {
+        public bool isHost;
         public int id;
         public string fullName;
         public string avatar;
     }
     public static List<User> participants;
+
+    public class Repo
+    {
+        public int id;
+        public string name;
+    }
 
     void Awake()
     {
@@ -43,24 +57,32 @@ public class Store : MonoBehaviour
         DontDestroyOnLoad(gameObject);
 
         code = Utilities.GetCode();
-        Debug.Log("CODE: " + code);
 
         // POST code to the server and GET user data
         if (code != null)
         {
+            Debug.Log("CODE: " + code);
             StartCoroutine(Utilities.PostCode(code));
         }
 
     }
 
     // DELEGATES && EVENTS
-    public delegate void ParticipantsChanged();
-    public static event ParticipantsChanged OnParticipantsChanged;
+    public delegate void ParticipantsAdded(User participant);
+    public static event ParticipantsAdded OnParticipantsAdded;
 
     public static void AddParticipant(User participant)
     {
         participants.Add(participant);
-        OnParticipantsChanged?.Invoke();
+        OnParticipantsAdded?.Invoke(participant);
     }
 
+    public delegate void ParticipantsRemoved(User participant);
+    public static event ParticipantsRemoved OnParticipantsRemoved;
+
+    public static void RemoveParticipant(User participant)
+    {
+        participants.Remove(participant);
+        OnParticipantsRemoved?.Invoke(participant);
+    }
 }
